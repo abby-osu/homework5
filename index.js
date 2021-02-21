b9ef39e8cfb016266302318564bd90fd137afa75
abby-osu <76971382+abby-osu@users.noreply.github.com>
2021-02-21 14:26:48 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,66 @@
+const express = require('express');
+
+
+const app= express();
+var handlebars = require('express-handlebars').create({defaultLayout:'main'});
+
+app.use(express.json())
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+app.set('port', 5207);
+app.use(express.urlencoded({
+    extended: true
+  }))
+
+
+
+app.get('/',function(req,res){
+    // data= {
+    //     title: "GET Request Received",
+    //     data:{
+    //         name='max',
+    //         age=20
+    //     }
+    // }
+
+    let data= {
+        title: "GET Request Received",
+        data:req.query
+    }
+    res.render('home.handlebars', data) //We can omit the .handlebars extension as we do below
+});
+
+app.post("/", function(req, res){
+    console.log(req.query);
+    console.log(req.body);
+    //   let data= {
+    //     title: "POST Request Received",
+    //     data:{ name: 'max', age: '20' },
+    //     body:{ data1: 'val1' }
+    // }
+
+    let data= {
+        title: "POST Request Received",
+        data:req.query,
+        body:req.body
+    }
+    res.render('post.handlebars', data) //We can omit the .handlebars extension as we do below
+})
+
+app.use(function(req,res){
+    res.status(404);
+    res.render('404');
+});
+
+app.listen(app.get('port'), function(){
+    console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
+});
+
+
+
+
+
+
+
+
+
